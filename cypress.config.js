73b4a20f0c276a2f5f77af52c43f9ace53bd7a10
ViganKrasniqi <93586888+ViganKrasniqi@users.noreply.github.com>
2023-02-26 20:48:40 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,12 +8,13 @@ module.exports = defineConfig({
     },
   },
   env: {
-    API_TOKEN : 'EAAAEVcX_LFvTxvV3enASd3lbqETb6VLZSbwglXSanjJrtthZqlsi92QuLf4yUSI',
     location: URL + 'v2/locations',
     catalog_list: URL + 'v2/catalog/list',
     catalog_batch_upsert: URL + 'v2/catalog/batch-upsert',
     catalog_create_category: URL + 'v2/catalog/create-category',
-    catalog_create_item: URL + 'v2/catalog/create-item'
+    catalog_create_item: URL + 'v2/catalog/create-item',
+    reports_item_sales: URL + 'v2/reports/item-sales',
+    reports_sales: URL + 'v2/reports/sales',
   },
   watchForFileChanges: false,
 });
