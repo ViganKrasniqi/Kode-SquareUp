fda8fa6ac8dade4a278784a2873cdf387d8fa4da
ViganKrasniqi <93586888+ViganKrasniqi@users.noreply.github.com>
2023-02-26 20:40:35 +0100

    Update constant.js

--- cypress/fixtures/constant.js
+++ cypress/fixtures/constant.js
@@ -11,3 +11,5 @@ export function requestOptions () {
 }
 
 export const API_TOKEN = "EAAAEVcX_LFvTxvV3enASd3lbqETb6VLZSbwglXSanjJrtthZqlsi92QuLf4yUSI"
+export const startDate = '2023-02-01T00:00:00Z';
+export const endDate = '2023-02-21T23:59:59Z';
