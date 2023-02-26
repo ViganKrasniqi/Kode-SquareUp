1022275c8f5d3eefaefbe3360588bd1f76d71267
ViganKrasniqi <93586888+ViganKrasniqi@users.noreply.github.com>
2023-02-26 20:50:29 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-export const API_TOKEN = 'EAAAEVcX_LFvTxvV3enASd3lbqETb6VLZSbwglXSanjJrtthZqlsi92QuLf4yUSI';
-
+import {API_TOKEN} from '../../fixtures/constant'
 
 Cypress.Commands.add("apiRequest", (method, url, body = null) => {
   const requestOptions = {
@@ -42,6 +15,3 @@ Cypress.Commands.add("apiRequest", (method, url, body = null) => {
   }
   return cy.request(requestOptions);
 });
-
-
-
